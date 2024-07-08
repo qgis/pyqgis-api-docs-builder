70239d30ffb8d2a0ecf78b80372e44b2e43f4b2c
Geo-Ninja <qgisninja@gmail.com>
2024-07-08 10:30:20 +0000

    Update docs

--- fix-versions.sh
+++ fix-versions.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-VERSIONS=(master 3.32 3.30 3.28 3.26 3.24 3.22 3.20 3.18 3.16 3.14 3.12 3.10 3.8 3.6 3.4 3.2 3.0)
+VERSIONS=(master 3.36 3.34 3.32 3.30 3.28 3.26 3.24 3.22 3.20 3.18 3.16 3.14 3.12 3.10 3.8 3.6 3.4 3.2 3.0)
 for v in "${VERSIONS[@]}"; do
   HTML="${HTML}\n      \n        <dd><a href=\"https://qgis.org/pyqgis/${v}\">${v}</a></dd>"
 done
