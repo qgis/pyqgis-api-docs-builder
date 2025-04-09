d922368a0149c75b117b0f4bbaedf025c61f2c25
Nyall Dawson <nyall.dawson@gmail.com>
2025-04-09 11:17:58 +1000

    Add utility method to get class from fully qualified attribute name

--- docs_builder/utils.py
+++ docs_builder/utils.py
@@ -2,6 +2,8 @@
 Contains utility functions for documentation logic
 """
 
+from typing import Any
+
 
 class Utils:
     """
@@ -34,3 +36,35 @@ class Utils:
             tokens.append(current_token.strip())
 
         return tokens
+
+    @staticmethod
+    def get_class_from_fully_qualified_attribute_name(
+        name: str, module_globals: dict[str, Any]
+    ) -> type | None:
+        """
+        Given the fully-qualified name of an attribute, returns the fully-qualified
+        name of the class that the object belongs to.
+        """
+        name_parts = name.split(".")
+        _class = None
+        if len(name_parts) > 1:
+            try_class_name_parts = name_parts[:]
+            while try_class_name_parts:
+                try:
+                    _class = module_globals[".".join(try_class_name_parts)]
+                    break
+                except KeyError:
+                    try_class_name_parts = try_class_name_parts[:-1]
+
+            if _class is not None:
+                while len(try_class_name_parts) < len(name_parts):
+                    try_class_name_parts.append(name_parts[len(try_class_name_parts)])
+                    if hasattr(_class, try_class_name_parts[-1]) and isinstance(
+                        getattr(_class, try_class_name_parts[-1]), type
+                    ):
+                        _class = getattr(_class, try_class_name_parts[-1])
+                    else:
+                        break
+                return _class
+
+        return None
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,6 +7,28 @@ import unittest
 from docs_builder.utils import Utils
 
 
+class TestClass:
+    attribute = "test"
+
+    class NestedClass:
+        nested_attribute = "nested"
+
+    def test_method(self):
+        pass
+
+
+class OuterModule:
+    class InnerClass:
+        inner_attribute = "inner"
+
+
+class ComplexHierarchy:
+    class Level1:
+        class Level2:
+            class Level3:
+                deep_attribute = "deep"
+
+
 class TestUtils(unittest.TestCase):
     """
     Test Utils class
@@ -49,6 +71,61 @@ class TestUtils(unittest.TestCase):
 
         # Whitespace handling
         self.assertEqual(Utils.split_to_tokens("  a  ,  b[c, d]  ,  e  "), ["a", "b[c, d]", "e"])
+
+    def test_get_class_from_fully_qualified_attribute_name(self):
+        """
+        Test Utils.get_class_from_fully_qualified_attribute_name
+        """
+
+        # Basic attribute lookup
+        result = Utils.get_class_from_fully_qualified_attribute_name(
+            "TestClass.attribute", globals()
+        )
+        self.assertEqual(result, TestClass)
+
+        # Nested class attribute
+        result = Utils.get_class_from_fully_qualified_attribute_name(
+            "TestClass.NestedClass.nested_attribute", globals()
+        )
+        self.assertEqual(result, TestClass.NestedClass)
+
+        # Double nested class
+        result = Utils.get_class_from_fully_qualified_attribute_name(
+            "OuterModule.InnerClass.inner_attribute", globals()
+        )
+        self.assertEqual(result, OuterModule.InnerClass)
+
+        # Nonexistent attribute
+        result = Utils.get_class_from_fully_qualified_attribute_name(
+            "NonexistentClass.attribute", globals()
+        )
+        self.assertIsNone(result)
+
+        # Nonexistent nested attribute
+        result = Utils.get_class_from_fully_qualified_attribute_name(
+            "TestClass.NonexistentNestedClass.attribute", globals()
+        )
+        self.assertEqual(result, TestClass)
+
+        # Single name no dots
+        result = Utils.get_class_from_fully_qualified_attribute_name("SingleName", globals())
+        self.assertIsNone(result)
+
+        # Empty string
+        result = Utils.get_class_from_fully_qualified_attribute_name("", globals())
+        self.assertIsNone(result)
+
+        # With method name instead of attribute
+        result = Utils.get_class_from_fully_qualified_attribute_name(
+            "TestClass.test_method", globals()
+        )
+        self.assertEqual(result, TestClass)
+
+        # With complicated hierarchy
+        result = Utils.get_class_from_fully_qualified_attribute_name(
+            "ComplexHierarchy.Level1.Level2.Level3.deep_attribute", globals()
+        )
+        self.assertEqual(result, ComplexHierarchy.Level1.Level2.Level3)
 
 
 if __name__ == "__main__":
