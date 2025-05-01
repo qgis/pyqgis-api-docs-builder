0481964348133f0d9b369f4ef5b0a2b3880af4a9
Nyall Dawson <nyall.dawson@gmail.com>
2025-05-01 15:24:32 +1000

    Fix crash on older QGIS versions

--- screenshots/gui/qgslayoutitempropertieswidget.py
+++ screenshots/gui/qgslayoutitempropertieswidget.py
@@ -1,6 +1,13 @@
 from pathlib import Path
 
-from qgis.core import QgsLayoutItemLabel, QgsPrintLayout, QgsProject
+from qgis.core import (
+    Qgis,
+    QgsLayoutItemLabel,
+    QgsLayoutPoint,
+    QgsLayoutSize,
+    QgsPrintLayout,
+    QgsProject,
+)
 from qgis.gui import QgsCollapsibleGroupBoxBasic, QgsLayoutItemPropertiesWidget
 from qgis.PyQt import sip
 from qgis.PyQt.QtWidgets import QWidget
@@ -11,7 +18,11 @@ from screenshots.utils import ScreenshotUtils
 def __generate_screenshots(dest_path: Path):
     project = QgsProject()
     layout = QgsPrintLayout(project)
+    layout.initializeDefaults()
     label_item = QgsLayoutItemLabel(layout)
+    label_item.setText("My label")
+    label_item.attemptResize(QgsLayoutSize(15, 10, Qgis.LayoutUnit.Centimeters))
+    label_item.attemptMove(QgsLayoutPoint(5, 7.5, Qgis.LayoutUnit.Centimeters))
     layout.addLayoutItem(label_item)
     widget = QgsLayoutItemPropertiesWidget(None, label_item)
     widget.setMasterLayout(layout)
@@ -27,5 +38,7 @@ def __generate_screenshots(dest_path: Path):
 
     im = ScreenshotUtils.capture_widget(widget, width=490)
     im.save((dest_path / "layoutitempropertieswidget.png").as_posix())
+    del widget
+    del layout
 
     return {"layoutitempropertieswidget.png": "QgsLayoutItemPropertiesWidget"}
