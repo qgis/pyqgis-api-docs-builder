fd51f4b066d29e87e761c1efa262b45bc989c274
Nyall Dawson <nyall.dawson@gmail.com>
2025-06-05 14:09:16 +1000

    Revert "Revert "Attempt to resurrect dropdown color button screenshot""
    This reverts commit 717cc523fc0b3c5b7af73b731d3ec91217303f27.

--- screenshots/gui/qgscolorbutton.py
+++ screenshots/gui/qgscolorbutton.py
@@ -14,11 +14,10 @@ def __generate_screenshots(dest_path: Path):
     im = ScreenshotUtils.capture_widget(widget, width=200)
     im.save((dest_path / "color_button.png").as_posix())
 
-    # triggers a crash in qgis! (requires https://github.com/qgis/QGIS/pull/61600)
-    # im = ScreenshotUtils.capture_toolbutton_with_dropdown(widget, width=250)
-    # im.save((dest_path / "color_button_expanded.png").as_posix())
+    im = ScreenshotUtils.capture_toolbutton_with_dropdown(widget, width=250)
+    im.save((dest_path / "color_button_expanded.png").as_posix())
 
     return {
         "color_button.png": "QgsColorButton in a default state",
-        #       "color_button_expanded.png": "QgsColorButton showing drop down menu",
+        "color_button_expanded.png": "QgsColorButton showing drop down menu",
     }
