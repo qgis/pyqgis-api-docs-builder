5ad8700cead6799b364566b8b3393393344c1495
Nyall Dawson <nyall.dawson@gmail.com>
2025-06-06 09:16:01 +1000

    Set preview rect for QgsProjectionSelectionTreeWidget

--- screenshots/gui/qgsprojectionselectiontreewidget.py
+++ screenshots/gui/qgsprojectionselectiontreewidget.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from qgis.core import QgsApplication, QgsCoordinateReferenceSystem
+from qgis.core import QgsApplication, QgsCoordinateReferenceSystem, QgsRectangle
 from qgis.gui import QgsProjectionSelectionTreeWidget
 
 from screenshots.utils import ScreenshotUtils
@@ -21,6 +21,7 @@ def __generate_screenshots(dest_path: Path):
     widget = QgsProjectionSelectionTreeWidget()
     crs = QgsCoordinateReferenceSystem.fromEpsgId(4326)
     widget.setCrs(crs)
+    widget.setPreviewRect(QgsRectangle(7.58, -38.16, 76.34, 39.08))
 
     im = ScreenshotUtils.capture_widget(widget, width=600, height=700)
     im.save((dest_path / "projectionselectiontreewidget.png").as_posix())
