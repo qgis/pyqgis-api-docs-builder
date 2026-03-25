2566f5b69465f5f88383e55e3d375e1b32be65c1
Denis Rouzaud <denis.rouzaud@gmail.com>
2026-03-25 13:46:06 +0100

    Revert PyQt5-only enum changes in screenshots
    The PyQt6-style enum access (Qt.BrushStyle.SolidPattern,
    Qt.AlignmentFlag.AlignHCenter) works with both PyQt5 and PyQt6
    via the qgis.PyQt compatibility layer.
    
    The getattr(.., 'value', ..) trick for titleBarState is needed
    because PyQt6 enums are not directly int-compatible.
    
    Tested on both qgis/qgis:3.44-noble (PyQt5) and
    qgis/qgis:4.0-questing (PyQt6).

--- screenshots/gui/qgsbrushstylecombobox.py
+++ screenshots/gui/qgsbrushstylecombobox.py
@@ -8,7 +8,7 @@ from screenshots.utils import ScreenshotUtils
 
 def __generate_screenshots(dest_path: Path):
     combo = QgsBrushStyleComboBox()
-    combo.setBrushStyle(Qt.SolidPattern)
+    combo.setBrushStyle(Qt.BrushStyle.SolidPattern)
 
     im_collapsed = ScreenshotUtils.capture_widget(combo)
     im_collapsed.save((dest_path / "qgsbrushstylecombobox_collapsed.png").as_posix())
